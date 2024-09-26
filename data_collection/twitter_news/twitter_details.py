4937531c62d1e51ef37180472999eb135c0fef04
HEMANTH <163557351+hemanth8705@users.noreply.github.com>
2024-09-26 13:23:52 +0530

    added twitter details.py

--- data_collection/twitter_news/twitter_details.py
+++ data_collection/twitter_news/twitter_details.py
@@ -2,10 +2,7 @@
 
 def get_details():
     user_name = "hemanth8705"
-<<<<<<< HEAD
     password = input("enter your password: ")
-=======
-    password = "__________"
->>>>>>> c9314480f832f2092b91aa9c139f76989fbcefec
+
 
     return (user_name , password)
