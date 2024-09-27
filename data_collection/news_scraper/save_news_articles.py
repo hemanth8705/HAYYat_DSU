e0292166f7f19706f0efd862dc4cbe8b82aa81bb
HEMANTH <163557351+hemanth8705@users.noreply.github.com>
2024-09-27 12:10:56 +0530

    added updated files for revision

--- data_collection/news_scraper/save_news_articles.py
+++ data_collection/news_scraper/save_news_articles.py
@@ -1,12 +1,12 @@
 from get_news_articles import bing_news_scraper
 
 def start_news_scraping(query):
-    df = bing_news_scraper(query, max_articles=1)  # Limit to maximum articles
+    df = bing_news_scraper(query, max_articles= 7)  # Limit to maximum articles
 
     filepath = r"../../scraped_data/news_articles.csv"
     df.to_csv(filepath , index= True)
 
 if __name__ == "__main__":
     # Example usage
-    query = "banglore weather"
+    query = "news about nithin ghatkari "
     start_news_scraping(query)
--- data_collection/twitter_news/twitter_details.py
+++ data_collection/twitter_news/twitter_details.py
@@ -1,8 +1,7 @@
-
+user_name = "hemanth8705"
 
 def get_details():
     user_name = "hemanth8705"
     password = input("enter your password: ")
-
 
     return (user_name , password)
